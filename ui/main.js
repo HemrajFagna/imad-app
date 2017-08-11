4cbcd5ab9dfe4f935e5143751da4b84456798055
HemrajFagna <15103352hemraj@gmail.com>
2017-08-11 09:55:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
 console.log('Loaded!');
+
+var element=document.getElementById("main-text");
+element.innerHTML="New Value";
+
+var img=document.getElementById("modi");
+marginLeft=0;
+function moveRight() {
+    marginLeft=margiLeft+2;
+    img.style.marginLeft='marginLeft'+'px';
+    
+}
+
+
+
+img.onClick=function() {
+    var interval=setInterval(moveRight,50);
+    
+};
+
