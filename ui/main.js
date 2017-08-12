f9511ca9ed02ed5903e82e45fb81bbcf37b5d7ba
HemrajFagna <15103352hemraj@gmail.com>
2017-08-12 13:36:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,8 +33,6 @@ submit.onClick= function() {
     for(var i=0; i<names.length;i++)
        list+="<li>"+names[i]+"</li>";
        
-       var ul=document.getElementById("namelist");
-       
-    
-    
-};
+      
+     };
+ var ul=document.getElementById("namelist");
