f43e8dba09906cd44c886f01d368f5b425323e84
HemrajFagna <15103352hemraj@gmail.com>
2017-08-12 13:33:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,21 @@ button.onClick= function () {
 
 request.open('GET','http://u15103352hemraj.imad.hasura-app.io/counter',true);
 request.send(null);
+
+
+
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit = document.getElementById("submit-btn");
+
+submit.onclick= function() {
+    var names=['name0','name1','name2','name3','name4'];
+    var list="";
+    for(var i=0; i<names.length;i++)
+       list+="<li>"+names[i]+"</li>";
+       
+       var ul=document.getElementById("namelist");
+       
+    
+    
+};
