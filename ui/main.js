2b204979736ba510acd32512a47c3fa154c52e8f
HemrajFagna <15103352hemraj@gmail.com>
2017-08-12 13:35:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ var nameInput=document.getElementById("name");
 var name=nameInput.value;
 var submit = document.getElementById("submit-btn");
 
-submit.onclick= function() {
+submit.onClick= function() {
     var names=['name0','name1','name2','name3','name4'];
     var list="";
     for(var i=0; i<names.length;i++)
